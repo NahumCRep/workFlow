d3d7f90cc03b779087080d919bc6e4d1e49f8aef
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-15 23:37:22 -0500

    loader added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ import { BiLoader } from 'react-icons/bi'
 const Loader = () => {
   return (
     <div className='w-full h-full flex justify-center items-center animate-spin'>
-        <BiLoader color='#1E5128' />
+        <BiLoader color='#1E5128' size={30} />
     </div>
   )
 }
