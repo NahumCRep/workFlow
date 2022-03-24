ccb360a6d4d7f313f6bce51684462e135bf1cdfd
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-23 21:33:27 -0500

    new api calls with axios

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,67 +1,101 @@
-import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { post } from '../../api'
 
 
-export const login = createAsyncThunk("user/login",async (credentials,thunkAPI)=>{
-    const response = await fetch("https://backendtzuzulcode.wl.r.appspot.com/auth/login",{
-            method:"POST",
-            credentials:'include',
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify({
-                email:credentials.email,
-                password:credentials.password
-            })
-        })
-    const data = await response.json()
-    if(data.message){
-        return thunkAPI.rejectWithValue(data.message)
-    }
+// export const login = createAsyncThunk("user/login",async (credentials,thunkAPI)=>{
+//     const response = await fetch("https://backendtzuzulcode.wl.r.appspot.com/auth/login",{
+//             method:"POST",
+//             credentials:'include',
+//             headers:{
+//                 "Content-Type":"application/json"
+//             },
+//             body:JSON.stringify({
+//                 email:credentials.email,
+//                 password:credentials.password
+//             })
+//         })
+//     const data = await response.json()
+//     if(data.message){
+//         return thunkAPI.rejectWithValue(data.message)
+//     }
 
+//     return data
+// })
+
+export const login = createAsyncThunk("user/login", async (credentials, thunkAPI) => {
+    const response = await post("/auth/login", {
+        email: credentials.email,
+        password: credentials.password
+    })
+    console.log('login response', response.data)
     //action.payload del reducer (fullfilled)
-    return data
+    return response.data
 })
 
-export const validate = createAsyncThunk("user/validate",async (credentials,thunkAPI)=>{
-    const response = await fetch("https://backendtzuzulcode.wl.r.appspot.com/auth/validate",{
-            method:"POST",
-            credentials:'include',
-            headers:{
-                "Content-Type":"application/json"
-            }
-        })
-    const data = await response.json()
-    console.log(data)
-    if(data.message){
-        // console.log("Lanzando error...")
-        return thunkAPI.rejectWithValue("Error de loggeo")
-    }
-    return data
+export const signup = createAsyncThunk("user/signup", async (credentials, thunkAPI) => {
+    const response = await post("/auth/signup", {
+        name: credentials.name,
+        birthday: credentials.birthday,
+        city: credentials.city,
+        email: credentials.email,
+        password: credentials.password
+    })
+    console.log('singup response', response.data)
+    return response.data
 })
 
-export const logout = createAsyncThunk("user/logout",async (credentials,thunkAPI)=>{
-    const response = await fetch("https://backendtzuzulcode.wl.r.appspot.com/auth/logout",{
-            method:"POST",
-            credentials:'include',
-            headers:{
-                "Content-Type":"application/json"
-            },
-        })
-    const data = await response.json()
-    return data
+// export const validate = createAsyncThunk("user/validate",async (credentials,thunkAPI)=>{
+//     const response = await fetch("https://backendtzuzulcode.wl.r.appspot.com/auth/validate",{
+//             method:"POST",
+//             credentials:'include',
+//             headers:{
+//                 "Content-Type":"application/json"
+//             }
+//         })
+//     const data = await response.json()
+//     console.log(data)
+//     if(data.message){
+//         // console.log("Lanzando error...")
+//         return thunkAPI.rejectWithValue("Error de loggeo")
+//     }
+//     return data
+// })
+
+export const validate = createAsyncThunk("user/validate", async (credentials, thunkAPI) => {
+    const response = await post("/auth/validate")
+    console.log('validacion', response.data)
+    return response.data
+})
+
+// export const logout = createAsyncThunk("user/logout", async (credentials, thunkAPI) => {
+//     const response = await fetch("https://backendtzuzulcode.wl.r.appspot.com/auth/logout", {
+//         method: "POST",
+//         credentials: 'include',
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//     })
+//     const data = await response.json()
+//     return data
+// })
+
+export const logout = createAsyncThunk("user/logout", async (credentials, thunkAPI) => {
+    const res = await post("/auth/logout")
+    return res.data
 })
 
 
 const userSlice = createSlice({
-    name:"user",
-    initialState:{
-        logged:false,
-        name:"",
-        loading:false,
-        error:true,
-        message:""
+    name: "user",
+    initialState: {
+        logged: false,
+        id: "",
+        name: "",
+        loading: false,
+        error: true,
+        message: ""
     },
-    reducers:{
+    reducers: {
         // login(state,action){
         //     state.logged = true
         //     state.name = action.payload
@@ -72,58 +106,86 @@ const userSlice = createSlice({
         // }
     },
     // Thunks
-    extraReducers(builder){
+    extraReducers(builder) {
         // LOGIN    
         builder.addCase(login.pending,(state,action)=>{
-            state.loading=true
+            state.loading = true
+            state.error = false
+            state.message = ""
+            state.name = ""
         })
 
         builder.addCase(login.fulfilled,(state,action)=>{
             state.loading = false
             state.logged = true
             state.error = false
-            state.name = action.payload.firstName
+            state.name = action.payload.name
+            state.id = action.payload.id
         })
 
         builder.addCase(login.rejected,(state,action)=>{
             state.loading = false
             state.error = true
-            state.message = action.payload
+            state.message = action.error.message
         })
 
-        // VALIDATE
+        //VALIDATE
         builder.addCase(validate.pending,(state,action)=>{
-            state.loading=true
+            state.loading = true
         })
 
         builder.addCase(validate.fulfilled,(state,action)=>{
-            state.loading = false
+            state.logged = true
+            state.name = action.payload?.user?.name
+            state.id = action.payload?.user?.id
             state.error = false
-            state.name = action.payload.firstName
+            state.loading = false
         })
 
         builder.addCase(validate.rejected,(state,action)=>{
+            state.logged = false
             state.loading = false
-            state.error = true
-            state.message = action.payload.message
         })
 
-        // LOGOUT
         builder.addCase(logout.pending,(state,action)=>{
-            state.loading=true
+            state.loading = true
         })
 
         builder.addCase(logout.fulfilled,(state,action)=>{
-            state.loading = false
-            state.error = false
             state.logged = false
             state.name = ""
+            state.id = ""
+            state.error = false
+            state.loading = false
+            state.message = ""
         })
 
         builder.addCase(logout.rejected,(state,action)=>{
+            state.error = true
+            state.logged = false
+            state.message = "Error"
+            state.loading = false
+        })
+
+        //SIGNUP
+        builder.addCase(signup.pending,(state,action)=>{
+            state.loading = true
+            state.error = false
+            state.message = ""
+            state.name = ""
+        })
+
+        builder.addCase(signup.fulfilled,(state,action)=>{
+            state.loading = false
+            state.logged = true
+            state.error = false
+            state.name = action.payload.name
+        })
+
+        builder.addCase(signup.rejected,(state,action)=>{
             state.loading = false
             state.error = true
-            state.message = action.payload.message
+            state.message = action.error.message
         })
     }
 })
