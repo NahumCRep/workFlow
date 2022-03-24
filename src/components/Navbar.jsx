5723d672f1602ab6d9f5c739e4da09d72ddee655
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-23 21:33:02 -0500

    new style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../features/user/userSlice';
 import { FaUserCircle } from 'react-icons/fa';
-import { BsGearFill } from 'react-icons/bs';
+// import { BsGearFill } from 'react-icons/bs';
+import { BiLogOut } from 'react-icons/bi'
 import { MdLogin } from 'react-icons/md';
 
 const Navbar = () => {
@@ -11,19 +12,6 @@ const Navbar = () => {
     const dispatch = useDispatch();
     const [isSettingsOpen, setIsSettingsOpen] = useState(false);
     const navigate = useNavigate();
-    console.log(name);
-    // const signOut = () => {
-    //     fetch('https://backendtzuzulcode.wl.r.appspot.com/auth/logout',
-    //         {
-    //             method: "POST",
-    //             credentials: 'include'
-    //         }
-    //     )
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             dispatch(logout())
-    //         })
-    // }
 
     const singOut = () => {
         dispatch(logout());
@@ -32,42 +20,44 @@ const Navbar = () => {
     }
 
     return (
-        <nav className="w-full h-16 bg-palette-dark flex justify-between items-center px-8">
+        <nav className="w-full h-16 bg-palette-dark fixed flex justify-between items-center px-8">
             <div className='w-[33%]'>
-                <Link to='/' className='w-auto' ><p className='w-auto font-righteous text-2xl text-palette-beige '><span className='text-palette-lightgreen'>Task</span>Flow</p></Link>
+                <div className='w-fit'>
+                    <Link to='/' className='w-auto' ><p className='w-auto font-righteous text-2xl text-palette-beige '><span className='text-palette-lightgreen'>Task</span>Flow</p></Link>
+                </div>
             </div>
-            <ul className='text-palette-beige flex font-jost gap-9 w-[33%] justify-center'>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to='/'>About</Link></li>
-                <li><Link to="/">Services</Link></li>
-                <li><Link to="/">Join</Link></li>
-            </ul>
-            <ul className='text-palette-beige flex gap-5 w-[33%] justify-end items-center'>
-                {/* {logged ? <li onClick={signOut}>Cerrar sesión</li> : <li className='nav_loggin_btn'><Link to="/login">Iniciar sesión</Link></li>} */}
-                <li><Link to='/dashboard'>Dashboard</Link></li>
-                {
-                    logged
-                        ? (
-                            <>
-                                <li><Link to='/dashboard'>Dashboard</Link></li>
-                                <li>
-                                    <div >
-                                        <span><FaUserCircle /> {name}</span>
+            <div className='w-[67%] flex justify-between items-center'>
+                <ul className='text-palette-beige flex font-jost gap-9 justify-center'>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to='/'>About</Link></li>
+                    <li><Link to="/">Services</Link></li>
+                    <li><Link to="/">Join</Link></li>
+                </ul>
+                <ul className='text-palette-beige flex gap-5 justify-end items-center'>
+                    {/* <li><Link to='/dashboard'>Dashboard</Link></li> */}
+                    {
+                        logged
+                            ? (
+                                <>
+                                    <li><Link to='/dashboard'>Dashboard</Link></li>
+                                    <li>
                                         <button
                                             className='flex'
-                                            onClick={() => setIsSettingsOpen(!isSettingsOpen)}
-                                        ><BsGearFill size={20} /></button>
-                                    </div>
-                                </li>
-                            </>
-                        )
-                        : <li><Link to="/login" className='flex items-center transition-all duration-700 ease-in-out hover:gap-2 gap-1 bg-palette-lightgreen p-2  rounded-xl'>Login <MdLogin /></Link></li>
-                }
+                                            onClick={() => singOut()}
+                                        ><BiLogOut size={20} /></button>
+                                    </li>
+                                </>
+                            )
+                            : (
+                                <>
+                                    <li><Link to="/signup">SignUp</Link></li>
+                                    <li><Link to="/login" className='flex items-center transition-all duration-700 ease-in-out hover:bg-palette-green gap-1 bg-palette-lightgreen p-2  rounded-xl'>Login <MdLogin /></Link></li>
+                                </>
+                            )
+                    }
+                </ul>
+            </div>
 
-            </ul>
-            {/* <div>
-                <button className='settins_logout' onClick={singOut}>Cerrar sesión</button>
-            </div> */}
         </nav>
     )
 }
