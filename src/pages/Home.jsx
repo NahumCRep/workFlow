2585e199f589d1d2cee961594bc55fa511a56b42
Nahum Casco <nahumcasco.v@gmail.com>
2022-03-29 00:11:19 -0500

    home header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { useRef } from 'react'
+import workflowImage from '../assets/images/workflow.svg'
 
 const Home = () => {
   const aboutRef = useRef(null)
   return (
     <section>
-    <div>Home</div>
-    <div ref={aboutRef}>about</div>
+      <header className='flex h-screen'>
+        <div className='w-1/2 bg-palette-lightgreen rounded-br-full flex justify-center items-center flex-col'>
+          <div className='w-[60%] flex flex-col'>
+            <h1 className='font-righteous text-6xl'>Do tasks in an easy way</h1>
+            <p className='font-righteous'>with</p>
+            <p className='text-4xl font-righteous'><span className='text-palette-beige'>Task</span>Flow</p>
+          </div>
+        </div>
+        <div className='w-1/2 flex justify-center items-center'>
+          <img src={workflowImage} alt='workFlow image' />
+        </div>
+      </header>
     </section>
   )
 }
